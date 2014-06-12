5bd52f7ff6c454f86471f12c3c8ff8fb97e2cb22
Lukas Platz-Bowden <bowden.lukas@gmail.com>
2014-06-12 19:05:47 +1000

    Initial commit
    Base project with basic movement

---
+++ Assets/Scripts/MainCameraTracking.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraTracking : MonoBehaviour {
+
+	public float yMargin = 1.0f;
+	public float ySmooth = 1.0f;
+
+	private Transform player;
+
+	void Awake () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	bool CheckYMargin()
+	{
+		return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+	}
+		
+	void FixedUpdate ()
+	{
+		TrackPlayer();
+	}
+
+	void TrackPlayer ()
+	{
+		float targetX = transform.position.x;
+		float targetY = transform.position.y;
+	
+		targetX = player.position.x;
+		
+	
+		if(CheckYMargin())
+			targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime);
+	
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+}
